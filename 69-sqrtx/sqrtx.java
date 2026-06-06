1e3e327033561820d281edbe8f32cd3640e25bfd
Garibcoder <130327540+Garibcoder@users.noreply.github.com>
2026-06-06 20:50:53 +0530

    Time: 1 ms (98.02%) | Memory: 42.6 MB (41.01%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x < 2) return x;
+        
+        int left = 1, right = x / 2;
+        int ans = 0;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long sq = (long) mid * mid; // overflow avoid
+            
+            if (sq == x) {
+                return mid;
+            } else if (sq < x) {
+                ans = mid;   // possible answer
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+}
